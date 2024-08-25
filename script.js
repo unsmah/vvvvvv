f4ec32e42a8f58973278f178c23f9c7f5ed03a47
unsmah <42187761+unsmah@users.noreply.github.com>
2024-08-25 19:39:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+  (function() {
+    const webAppUrl = 'https://script.google.com/macros/s/AKfycbwKf-nFwfzfHLNACumV16XWTH5RXMwicIFsylqtVPWBU0JgiaxQ0LzY1REFLTi-N_aw/exec';
+    
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      document.getElementById('userName').textContent = `${user.firstName} ${user.lastName}`;
+      document.getElementById('profileLink').href = `URL_TO_USER_PROFILE_PAGE/${user.id}`;
+    } else {
+      document.querySelector('.dropdown').style.display = 'none';
+    }
+
+    function logout() {
+      const user = JSON.parse(localStorage.getItem('user'));
+      if (user) {
+        fetch(`${webAppUrl}?action=logout&id=${user.id}`)
+          .then(() => {
+            localStorage.removeItem('user');
+            localStorage.removeItem('redirected');
+            window.location.reload();
+          })
+          .catch(error => {
+            console.error('Logout error:', error);
+          });
+      }
+    }
+
+    // Toggle dropdown on click
+    document.getElementById('userButton').addEventListener('click', function() {
+      document.getElementById('myDropdown').classList.toggle('show');
+    });
+
+    // Close the dropdown if the user clicks outside of it
+    window.addEventListener('click', function(event) {
+      if (!event.target.matches('.dropbtn') && !event.target.matches('.avatar')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    });
+
+    window.logout = logout;
+  })();
